a49e96fc63100d945383db0042352bd3faafb165
Aiyana Ball <ballaiyana@gmail.com>
2023-06-16 15:06:06 -0700

    Added proptype

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, updateTaskComplete}) => {
+  // const [complete, setComplete] = useState(isComplete);
+  // const updateTaskStatus = () => {
+  //   updateTaskComplete(id);
+  // };
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => updateTaskComplete(id)}
       >
         {title}
       </button>
@@ -24,6 +27,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateTaskComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
