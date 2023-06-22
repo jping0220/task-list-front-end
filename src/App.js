e473b0e8b6dcbc20279984766d3d54f7c5ec7e34
Jiaping Chen <jiapingchen17@gmail.com>
2023-06-21 17:17:41 -0700

    modified

--- src/App.js
+++ src/App.js
@@ -122,3 +122,52 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+const NewTaskForm = ({ addTask }) => {
+    const [title, setTitle] = useState('');
+    const [isComplete, setIsComplete] = useState(false);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = {
+            title: title,
+            isComplete: isComplete,
+        };
+        addTask(newTask);
+        setTitle('');
+        setIsComplete(false);
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="title"> Title:</label>
+            <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}></input>
+            <label htmlFor="isComplete" >Is Complete:</label>
+            <input
+                type="text"
+                id="isComplete"
+                checked={isComplete}
+                onChange={(event) => setIsComplete(event.target.checked)} />
+            
+            <button type="submit">  Add Task </button>
+        </form>
+    );
+};
+NewTaskForm.propTypes = {
+    addTask: PropTypes.func.isRequired,
+};
+
+export default NewTaskForm;
